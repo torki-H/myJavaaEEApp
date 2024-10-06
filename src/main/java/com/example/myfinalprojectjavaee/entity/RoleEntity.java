44de3fd1ebbbe5e57a245cd4a5b6b98c708779fe
samaes.ir <hossientorki0@gmail.com>
2024-10-06 16:46:05 +0330

    security realm

---
+++ src/main/java/com/example/myfinalprojectjavaee/entity/RoleEntity.java
@@ -0,0 +1,26 @@
+//package com.example.myfinalprojectjavaee.entity;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import lombok.ToString;
+//
+//@Entity(name = "role")
+//@Table(name = "rolejavaeefinal")
+//@Data
+//@ToString
+//@NoArgsConstructor
+//public class RoleEntity {
+//
+//    @Id
+//    @Column()
+//    private int Id;
+//    @Column
+//    private String user_name;
+//    @Column
+//    private String role_name;
+//
+//}
--- src/main/java/com/example/myfinalprojectjavaee/entity/UserEntity.java
+++ src/main/java/com/example/myfinalprojectjavaee/entity/UserEntity.java
@@ -15,16 +15,17 @@
 //public class UserEntity
 //   {
 //    @Id
-//    @Column(name = "UsrID",columnDefinition = "NUMBER")
+//    @Column()
+//    /*@Column(name = "UsrID",columnDefinition = "NUMBER")
 //    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "usrSeq")
-//    @SequenceGenerator(name = "usrSeq",allocationSize = 1,sequenceName = "USERENTITY_SEQ")
+//    @SequenceGenerator(name = "usrSeq",allocationSize = 1,sequenceName = "USERENTITY_SEQ")*/
 //    private int Id;
 //    @Column
-//    private String userName;
+//    private String user_name;
 //    @Column
-//    private String password;
+//    private String user_pass;
 //
 //
-//    @OneToOne(mappedBy = "user") // مشخص کردن اینکه اینجا مالک نیستیم
-//    private EmployeeEntity employee;
+//    /*@OneToOne(mappedBy = "user") // مشخص کردن اینکه اینجا مالک نیستیم
+//    private EmployeeEntity employee;*/
 //}
